c0c9f464828e68e92e0dd95670d088db2cfefe0a
Andrew Gu <andgu@fb.com>
2024-01-21 08:22:04 -0800

    [FSDP2] Added initial `mesh` arg and helper structures
    ghstack-source-id: 61556cbe0e58e7536f7d5e46c5b617d5cd14317f
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/117817

--- torch/distributed/_composable/fsdp/_fsdp_init.py
+++ torch/distributed/_composable/fsdp/_fsdp_init.py
@@ -1,6 +1,9 @@
 import torch
+import torch.distributed as dist
 
 from torch._prims_common import DeviceLikeType
+
+from torch.distributed._tensor import DeviceMesh, init_device_mesh
 
 
 def _normalize_device(device: DeviceLikeType) -> torch.device:
@@ -16,3 +19,14 @@ def _normalize_device(device: DeviceLikeType) -> torch.device:
         return torch.device(device)
     else:
         raise TypeError(f"Invalid type for device {device}: {type(device)}")
+
+
+def _init_default_fully_shard_mesh(device_type: str) -> DeviceMesh:
+    """The default fully-shard mesh shards over the global mesh."""
+    default_pg = dist.distributed_c10d._get_default_group()
+    mesh = init_device_mesh(
+        device_type=device_type,
+        mesh_shape=(default_pg.size(),),
+        mesh_dim_names=("dp_shard",),
+    )
+    return mesh
