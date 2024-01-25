8d8210c0e7d2a4d21c542d85b89eec150d233049
Andrew Gu <andgu@fb.com>
2024-01-25 08:49:02 -0800

    [FSDP2] Added `_get_managed_modules`/`_get_managed_states` for init
    ghstack-source-id: 038fb5261bc7dbd945fca132a24c8a783365350d
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/117852

--- test/distributed/_composable/fsdp/test_fully_shard_init.py
+++ test/distributed/_composable/fsdp/test_fully_shard_init.py
@@ -1,12 +1,19 @@
 # Owner(s): ["oncall: distributed"]
 
 import unittest
+from typing import List
 
 import torch
+import torch.nn as nn
 from _test_fully_shard_common import MLP
+from torch.distributed._composable import replicate
 
 from torch.distributed._composable.fsdp import fully_shard
-from torch.distributed._composable.fsdp._fsdp_init import _normalize_device
+from torch.distributed._composable.fsdp._fsdp_init import (
+    _get_managed_modules,
+    _get_managed_states,
+    _normalize_device,
+)
 from torch.distributed.device_mesh import init_device_mesh
 from torch.testing._internal.common_cuda import TEST_CUDA
 from torch.testing._internal.common_fsdp import FSDPTestMultiThread
@@ -66,6 +73,96 @@ class TestFullyShardMeshArg(FSDPTestMultiThread):
         regex = "device and mesh must be of the same type but got cpu for device and cuda for mesh"
         with self.assertRaisesRegex(ValueError, regex):
             fully_shard(model, mesh=mesh, device="cpu")
+
+
+class TestFullyShardManagedModulesAndStates(FSDPTestMultiThread):
+    """Tests getting the managed modules/states for a ``fully_shard`` module."""
+
+    @property
+    def world_size(self) -> int:
+        return 1
+
+    @unittest.skipIf(not TEST_CUDA, "no cuda")
+    def test_managed_modules_single(self):
+        model = MLP(8)
+        # Assume calling `fully_shard` on `model`
+        managed_modules = _get_managed_modules(model)
+        expected_managed_modules = list(model.modules())
+        self._check_managed_modules(managed_modules, expected_managed_modules)
+
+    @unittest.skipIf(not TEST_CUDA, "no cuda")
+    def test_managed_modules_nested(self):
+        model = nn.Sequential(*[MLP(8) for _ in range(2)])
+        fully_shard(model[0])
+        # Assume calling `fully_shard` on `model`
+        managed_modules = _get_managed_modules(model)
+        expected_managed_modules = list(model[1].modules()) + [model]
+        self._check_managed_modules(managed_modules, expected_managed_modules)
+
+    @unittest.skipIf(not TEST_CUDA, "no cuda")
+    def test_managed_modules_nested_fully_shard_and_replicate(self):
+        model = nn.Sequential(*[MLP(8) for _ in range(3)])
+        replicate(model[0])
+        fully_shard(model[2])
+        # Assume calling `fully_shard` on `model`
+        managed_modules = _get_managed_modules(model)
+        expected_managed_modules = list(model[1].modules()) + [model]
+        self._check_managed_modules(managed_modules, expected_managed_modules)
+
+    @unittest.skipIf(not TEST_CUDA, "no cuda")
+    def test_managed_modules_duplicate(self):
+        mlp = MLP(8)
+        model = nn.Sequential(mlp, mlp)  # duplicate MLP
+        # Assume calling `fully_shard` on `model`
+        managed_modules = _get_managed_modules(model)
+        # Check that the duplicate module is only counted once
+        expected_managed_modules = list(mlp.modules()) + [model]
+        self._check_managed_modules(managed_modules, expected_managed_modules)
+
+    def _check_managed_modules(
+        self,
+        managed_modules: List[nn.Module],
+        expected_managed_modules: List[nn.Module],
+    ):
+        self.assertEqual(len(managed_modules), len(expected_managed_modules))
+        # Check set comparison since we do not require anything about the order
+        self.assertEqual(set(managed_modules), set(expected_managed_modules))
+
+    @unittest.skipIf(not TEST_CUDA, "no cuda")
+    def test_managed_states_shared_params_and_buffers(self):
+        model = nn.Sequential(*[MLP(8, with_buffer=True) for _ in range(3)])
+        model[0].in_proj.weight = model[1].in_proj.weight
+        model[2].in_proj.weight = model[1].in_proj.weight
+        model[1].buffer = model[2].buffer
+        # Assume calling `fully_shard` on `model`
+        managed_modules = _get_managed_modules(model)
+        params, buffers = _get_managed_states(managed_modules)
+        expected_params = list(model.parameters())  # de-dups shared
+        expected_buffers = list(model.buffers())  # de-dups shared
+        self._check_managed_states(params, buffers, expected_params, expected_buffers)
+
+    @unittest.skipIf(not TEST_CUDA, "no cuda")
+    def test_managed_states_nested_fully_shard(self):
+        model = nn.Sequential(*[MLP(8, with_buffer=True) for _ in range(2)])
+        fully_shard(model[0])
+        # Assume calling `fully_shard` on `model`
+        managed_modules = _get_managed_modules(model)
+        params, buffers = _get_managed_states(managed_modules)
+        expected_params = list(model[1].parameters())
+        expected_buffers = list(model[1].buffers())
+        self._check_managed_states(params, buffers, expected_params, expected_buffers)
+
+    def _check_managed_states(
+        self,
+        managed_params: List[nn.Parameter],
+        managed_buffers: List[torch.Tensor],
+        expected_managed_params: List[nn.Parameter],
+        expected_managed_buffers: List[torch.Tensor],
+    ):
+        self.assertEqual(len(managed_params), len(expected_managed_params))
+        self.assertEqual(len(managed_buffers), len(expected_managed_buffers))
+        self.assertEqual(set(managed_params), set(expected_managed_params))
+        self.assertEqual(set(managed_buffers), set(expected_managed_buffers))
 
 
 if __name__ == "__main__":
--- torch/distributed/_composable/fsdp/_fsdp_init.py
+++ torch/distributed/_composable/fsdp/_fsdp_init.py
@@ -1,9 +1,15 @@
+from typing import List, Set, Tuple
+
 import torch
 import torch.distributed as dist
+import torch.nn as nn
 
 from torch._prims_common import DeviceLikeType
 
 from torch.distributed._tensor import DeviceMesh, init_device_mesh
+
+from ._fsdp_common import _is_composable_with_fsdp
+from ._fsdp_state import _get_module_fsdp_state
 
 
 def _normalize_device(device: DeviceLikeType) -> torch.device:
@@ -28,3 +34,50 @@ def _init_default_fully_shard_mesh(device_type: str) -> DeviceMesh:
     default_pg = dist.distributed_c10d._get_default_group()
     mesh = init_device_mesh(device_type=device_type, mesh_shape=(default_pg.size(),))
     return mesh
+
+
+def _get_managed_modules(root_module: nn.Module) -> List[nn.Module]:
+    modules: List[nn.Module] = []
+    # Track visisted modules to avoid visiting shared modules multiple times
+    visited_modules: Set[nn.Module] = set()
+
+    def dfs(module: nn.Module) -> None:
+        """
+        Runs a DFS to collect managed modules, not recursing into modules with
+        a non-composable API or ``fully_shard`` already applied.
+        """
+        if not _is_composable_with_fsdp(module):
+            return
+        elif module is not root_module and _get_module_fsdp_state(module) is not None:
+            return  # nested `fully_shard` module
+        visited_modules.add(module)
+        for submodule in module.children():
+            if submodule is not None and submodule not in visited_modules:
+                dfs(submodule)
+        modules.append(module)
+
+    dfs(root_module)
+    return modules
+
+
+def _get_managed_states(
+    modules: List[nn.Module],
+) -> Tuple[List[nn.Parameter], List[torch.Tensor]]:
+    params: List[nn.Parameter] = []
+    buffers: List[torch.Tensor] = []
+    # Track visited parameters/buffers to avoid visiting shared parameters and
+    # buffers multiple times
+    visited_params: Set[nn.Parameter] = set()
+    visited_buffers: Set[torch.Tensor] = set()
+    for module in modules:
+        for param_name, param in module.named_parameters(recurse=False):
+            if param in visited_params:
+                continue
+            params.append(param)
+            visited_params.add(param)
+        for buffer_name, buffer in module.named_buffers(recurse=False):
+            if buffer in visited_buffers:
+                continue
+            buffers.append(buffer)
+            visited_buffers.add(buffer)
+    return params, buffers
