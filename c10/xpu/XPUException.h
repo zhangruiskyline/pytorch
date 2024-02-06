f924765dd325982f5c4377d02237ed6ca9259841
Yu, Guangye <guangye.yu@intel.com>
2024-02-06 09:41:30 +0000

    [1/2] Intel GPU Runtime Upstreaming for Stream
    ghstack-source-id: 42a5bb97ec1f613878308d60fa8cfa2d0ad130e2
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/117611

---
+++ c10/xpu/XPUException.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <c10/util/Exception.h>
+#include <sycl/sycl.hpp>
+
+namespace c10::xpu {
+
+static sycl::async_handler asyncHandler = [](sycl::exception_list el) {
+  for (auto& e : el) {
+    try {
+      std::rethrow_exception(e);
+    } catch (sycl::exception& e) {
+      TORCH_WARN("SYCL Exception: ", e.what());
+      throw;
+    }
+  }
+};
+
+} // namespace c10::xpu
