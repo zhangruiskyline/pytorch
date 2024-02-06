62deaeabc9dcc38d6c5c6d37258ebc5322ac3052
Tianyu Liu <lty@fb.com>
2024-02-05 23:56:43 -0800

    [dtensor] add op support for nll_loss_backward
    ghstack-source-id: 6ef1a0b4f230b8188167b1202bb563afde19086f
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/119256

--- test/distributed/_tensor/test_math_ops.py
+++ test/distributed/_tensor/test_math_ops.py
@@ -145,15 +145,19 @@ class DistMathOpsTest(DTensorTestBase):
             (3, (8, channel_size, 12), (8, 12)),  # calling aten.nll_loss2d_forward
         ]
         for input_ndim, input_size, target_size in test_setup:
-            x = torch.rand(*input_size, device=self.device_type)
+            x = torch.rand(*input_size, device=self.device_type, requires_grad=True)
             target = torch.randint(channel_size, target_size, device=self.device_type)
             dist_target = distribute_tensor(target, device_mesh, [Replicate()])
 
             shard_dims = list(range(input_ndim))
             reductions = ["none", "mean", "sum"]
             for shard_dim, reduction in itertools.product(shard_dims, reductions):
-                y = torch.nn.functional.nll_loss(x, target, reduction=reduction)
                 dist_x = distribute_tensor(x, device_mesh, [Shard(shard_dim)])
+                y = torch.nn.functional.nll_loss(x, target, reduction=reduction)
+                if reduction == "none":
+                    y.sum().backward()
+                else:
+                    y.backward()
                 with comm_mode:
                     dist_y = torch.nn.functional.nll_loss(
                         dist_x, dist_target, reduction=reduction
@@ -180,6 +184,20 @@ class DistMathOpsTest(DTensorTestBase):
                         else:
                             self.assertTrue(dist_y.placements[0].is_partial())
                         self.assertEqual(dist_y.full_tensor(), y)
+
+                    if reduction == "none":
+                        dist_y.sum().backward()
+                    else:
+                        dist_y.backward()
+                    if shard_dim == channel_dim:
+                        self.assertTrue(dist_x.grad.placements[0].is_replicate())
+                        self.assertEqual(dist_x.grad.to_local(), x.grad)
+                    else:
+                        self.assertTrue(
+                            dist_x.grad.placements[0].is_shard(dim=shard_dim)
+                        )
+                        self.assertEqual(dist_x.grad.full_tensor(), x.grad)
+                    x.grad.zero_()
 
     @with_comms
     def test_full_shard_math_ops(self):
